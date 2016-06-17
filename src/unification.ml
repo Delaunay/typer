9a13dd1a68c359a883bdaf3210c06c76989a15df
n3f4s <tiv.crb@gmail.com>
2016-06-17 17:59:13 +0200

    debug mutual recursion and cons param
    cons param -> add missing cons parameter in match
    mutual recursion -> use 'and' instead of 'let' to allow mutual recusion

--- src/unification.ml
+++ src/unification.ml
@@ -123,32 +123,32 @@ and _unify_call (call: lexp) (lxp: lexp) (subst: substitution) : return_type =
 (** Unify a Lambda and a lexp if possible
  * See above for result
  *)
-let _unify_lambda (lambda: lexp) (lxp: lexp) (subst: substituion) : return_type =
-  match (lambda, lexp) with
+and _unify_lambda (lambda: lexp) (lxp: lexp) (subst: substitution) : return_type =
+  match (lambda, lxp) with
   | (Lambda (var_kind1, _, ltype1, lexp1), Lambda (var_kind2, _, ltype2, lexp2))
     -> if var_kind1 = var_kind2
     then _unify_inner_arrow ltype1 lexp1 ltype2 lexp2 subst
     else None
-  | (Lambda, Var)   -> (subst, (lambda, lexp))
-  | (Lambda, Let)   -> (subst, (lambda, lexp))
-  | (Lambda, Arrow) -> (subst, (lambda, lexp)) (* ?? *)
-  | (Lambda, Call)  -> (subst, (lambda, lexp))
-  | (Lambda, _)     -> unify lexp lambda subst
-  | (_, _)          -> None
+  | (Lambda _, Var _)   -> Some ((subst, [(lambda, lxp)]))
+  | (Lambda _, Let _)   -> Some ((subst, [(lambda, lxp)]))
+  | (Lambda _, Arrow _) -> Some ((subst, [(lambda, lxp)])) (* ?? *)
+  | (Lambda _, Call _)  -> Some ((subst, [(lambda, lxp)]))
+  | (Lambda _, _)       -> unify lxp lambda subst
+  | (_, _)              -> None
 
 (** Unify a Metavar and a lexp if possible
  * See above for result
  * Metavar is the 'end' of the rules i.e. : it can call unify with his argument (re-ordered)
  *)
-let _unify_metavar (meta: lexp) (lxp: lexp) (subst: substitution) : return_type =
+and _unify_metavar (meta: lexp) (lxp: lexp) (subst: substitution) : return_type =
   match (meta, lxp) with
-  | (Metavar val1, Metavar val2) ->
+  | (Metavar (val1, _, _), Metavar (val2, _, _)) ->
     if val1 = val2
-    then (add_substitution meta lxp subst, ())
+    then Some ((add_substitution meta subst, []))
     else None
-  | (Metavar v, _) -> (
-      match find_or_none v subst with
-      | None          -> (associate v lxp subst, ())
+  | (Metavar (v, _, _), _) -> (
+      match find_or_none meta subst with
+      | None          -> Some ((associate v lxp subst, []))
       | Some (lxp_)   -> unify lxp_ lxp subst) (*Not sure if it's the expected behavior*)
   | (_, _) -> None
 
@@ -158,21 +158,21 @@ let _unify_metavar (meta: lexp) (lxp: lexp) (subst: substitution) : return_type 
                      else None
  * (_, _) -> None
  *)
-let rec _unify_arrow (arrow: lexp) (lxp: lexp) (subst: substitution)
+and _unify_arrow (arrow: lexp) (lxp: lexp) (subst: substitution)
   : return_type =
   match (arrow, lxp) with
   (*?????*)
-  | (Arrow (_, _, ltype1, lexp1), Arrow (_, _, ltype2, lexp2))
+  | (Arrow (var_kind1, _, ltype1, _, lexp1), Arrow (var_kind2, _, ltype2, _, lexp2))
     -> if var_kind1 = var_kind2
-    then (match _unify_inner_arrow ltype1 lexp1 ltype2 lexp2 susbt with
-        | Some -> (add_substitution arrow subst, ())
+    then (match _unify_inner_arrow ltype1 lexp1 ltype2 lexp2 subst with
+        | Some _ -> Some ((add_substitution arrow subst, []))
         | None -> None)
     else None
-  | (Arrow, _) -> unfiy lxp arrow subst
+  | (Arrow _, _) -> unify lxp arrow subst
   | (_, _) -> None
 
 (** Unify lexp & ltype (Arrow (_,_,ltype, lexp)) of two Arrow*)
-let _unify_inner_arrow (lt1: lexp) (lxp1: lexp)
+and _unify_inner_arrow (lt1: lexp) (lxp1: lexp)
     (lt2: lexp) (lxp2: lexp) (subst: substitution): return_type =
   match unify lt1 lt2 subst with
   | Some (subst_, const) -> ( (*bracket for formating*)
@@ -186,74 +186,52 @@ let _unify_inner_arrow (lt1: lexp) (lxp1: lexp)
  * (Var, Metavar) -> unify_metavar Metavar var subst
  * (_, _) -> None
  *)
-let _unify_var (var: lexp) (r: lexp) (subst: substitution) : return_type =
+and _unify_var (var: lexp) (r: lexp) (subst: substitution) : return_type =
   match (var, r) with
   | (Var (_, idx1), Var (_, idx2))
-    -> if idx1 = idx2 then (add_substitution var subst, ())
+    -> if idx1 = idx2 then Some ((add_substitution var subst, []))
     else None
-  | (Var, _) -> unify r var subst (*returns to unify*)
+  | (Var _, _) -> unify r var subst (*returns to unify*)
   | (_, _)   -> None
 
 (** Unify two Imm if they match <=> Same type and same value
  * Add one of the Imm (the first arguement) to the substitution *)
-let _unify_imm (l: lexp) (r: lexp) (subst: substitution) : return_type =
+and _unify_imm (l: lexp) (r: lexp) (subst: substitution) : return_type =
   match (l, r) with
   | (Imm (String (_, v1)), Imm (String (_, v2)))
-    -> if v1 = v2 then (add_substitution l subst, ())
+    -> if v1 = v2 then Some ((add_substitution l subst, []))
     else None
   | (Imm (Integer (_, v1)), Imm (Integer (_, v2)))
-    -> if v1 = v2 then (add_substitution l subst, ())
+    -> if v1 = v2 then Some ((add_substitution l subst, []))
     else None
   | (Imm (Float (_, v1)), Imm (Float (_, v2)))
-    -> if v1 = v2 then (add_substitution l subst, ())
+    -> if v1 = v2 then Some ((add_substitution l subst, []))
     else None
+  | (Imm _, Imm _) -> None
+  | (Imm _, _) -> unify r l subst
   | (_, _) -> None
 
 (** Unify a builtin (bltin) and a lexp (lxp) if it is possible
  * If the two arguments are builtin, unify based on name
  * If it's a Builtin and an other lexp, unify lexp part of Builtin with the lexp
 *)
-let _unify_builtin (bltin: lexp) (lxp: lexp) (subst: substitution) : return_type =
+and _unify_builtin (bltin: lexp) (lxp: lexp) (subst: substitution) : return_type =
   match (bltin, lxp) with
   | (Builtin ((_, name1), _), Builtin ((_, name2),_))
-    -> if name1 = name2 then (add_substitution l subst, ())
+    -> if name1 = name2 then Some ((add_substitution lxp subst, []))
     else None (* assuming that builtin have unique name *)
   | (Builtin (_, lxp_), _) -> (match unify lxp lxp subst with
       | None -> None
-      | Some (_, c)-> (add_substitution bltin subst, c))
+      | Some (_, c)-> Some ((add_substitution bltin subst, c)))
   | (_, _) -> None
 
 (** Unify a Let (let_) and a lexp (lxp), if possible
  * Unify the left lexp part of the Let (Let (_, _, lxp)) with the lexp
  *)
-let _unify_let (let_: lexp) (lxp: lexp) (subst: substitution) : return_type =
+and _unify_let (let_: lexp) (lxp: lexp) (subst: substitution) : return_type =
   match let_ with (* Discard the middle part of Let : right behavior ? *)
   | Let (_, _, lxp_) -> (match unify lxp_ lxp subst with
       | None -> None
-      | Some (_, c) -> (add_substitution let_ subst, c))
+      | Some (_, c) -> Some ((add_substitution let_ subst, c)))
   | _ -> None
 
-(** Generate the next metavar by taking the highest value and
- * adding it one
- *)
-let add_substitution (lxp: lexp) ((subst, max_): substitution) : substitution =
-  associate (max_ + 1) lxp (subst, max_)
-
-(** If key is in map returns the value associated
- * else returns None
- *)
-let find_or_none (value: lexp) ((map, max_): substitution) : lexp option =
-  match value with
-  | Metavar idx -> (if max_ < idx (* 0 < keys <= max_ *)
-                    then None
-                    else (if VMap.mem idx map
-                           then Some ((VMap.find idx map, max_))
-                           else None))
-  | _ -> None
-
-(** Alias for VMap.add*)
-let associate (meta: int) (lxp: lexp) ((subst, max_): substitution)
-  : substitution =
-  (VMap.add meta lexp subst, (max max_ meta ))
-
-let empty_subst = (VMap.empty, 0)
