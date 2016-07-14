9d83a8d4be5c21a2cfbd5ef6c02fe9e3a62fb239
n3f4s <tiv.crb@gmail.com>
2016-07-14 22:25:41 +0200

    move inversion function out of unification

--- src/unification.ml
+++ src/unification.ml
@@ -1,6 +1,7 @@
 
 open Lexp
 open Sexp
+open Inverse_subst
 
 module VMap = Map.Make (struct type t = int let compare = compare end)
 
@@ -230,18 +231,6 @@ and _unify_lambda (lambda: lexp) (lxp: lexp) (subst: substitution) : return_type
  * metavar   , lexp               -> OK
 *)
 and _unify_metavar (meta: lexp) (lxp: lexp) (subst: substitution) : return_type =
-  (** s:S.subst -> l:lexp -> s':S.subst where l[s][s'] = l *)
-  let rec inverse subst =
-    match subst with
-    | S.Identity -> Some S.Identity
-    | S.Shift (subst, shift_idx) -> (match inverse subst with
-        | Some s' -> Some (S.Shift (s', -shift_idx))
-        | None -> None)
-    | S.Cons (lxp, s') -> (match inverse s' with
-      | Some s -> Some( S.Cons (lxp,  s)) (* ?? *)
-      | None -> None)
-    (*| _ -> None*)
-  in
   let find_or_unify metavar value lxp s =
     match find_or_none metavar s with
     | Some (lxp_)   -> unify lxp_ lxp s
