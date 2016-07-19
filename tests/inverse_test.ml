27fc552db4dc7ccc9c0a3f3cea5e3e9f5f110173
n3f4s <tiv.crb@gmail.com>
2016-07-19 22:13:47 +0200

    Add function for creating test input

--- tests/inverse_test.ml
+++ tests/inverse_test.ml
@@ -23,93 +23,36 @@ open Debug_fun
 let mkShift2 shift subst =
   S.Shift (subst, shift)
 
+let rec mkTestSubst lst =
+  match lst with
+  | (var, shift)::tail -> S.Cons (mkVar var,
+                                  mkShift2 shift (mkTestSubst tail))
+  | [] -> S.Identity
+
 let input =
-  (S.Cons (mkVar 0, (* Seems to work*)
-           mkShift2 3 (S.Cons (mkVar 2,
-                               (mkShift2 2 (S.Cons (mkVar 3,
-                                                    mkShift2 0 S.Identity)
-                                           )
-                               )
-                              )
-                      )
-          )
-  )::
-  (S.Cons (mkVar 1, (* Seems to work *)
-           mkShift2 3 (S.Cons (mkVar 2,
-                               (mkShift2 2 (S.Cons (mkVar 3,
-                                                    mkShift2 0 S.Identity)
-                                           )
-                               )
-                              )
-                      )
-          )
-  )::
-  (S.Cons (mkVar 4,  (* Go completly wrong -> indices not in order -> should fail ?*)
-           mkShift2 3 (S.Cons (mkVar 2,
-                               (mkShift2 2 (S.Cons (mkVar 3,
-                                                    mkShift2 0 S.Identity)
-                                           )
-                               )
-                              )
-                      )
-          )
-  )::
-  (S.Cons (mkVar 1,  (* Seems to work *)
-           mkShift2 3 (S.Cons (mkVar 2,
-                               (mkShift2 2 (S.Cons (mkVar 4,
-                                                    mkShift2 0 S.Identity)
-                                           )
-                               )
-                              )
-                      )
-          )
-  )::
-  (S.Cons (mkVar 0,  (* Seems to work *)
-           mkShift2 3 (S.Cons (mkVar 2,
-                               (mkShift2 2 (S.Cons (mkVar 4,
-                                                    mkShift2 0 S.Identity)
-                                           )
-                               )
-                              )
-                      )
-          )
-  )::
-  (S.Cons (mkVar 0,  (* Seems to work *)
-           mkShift2 3 (S.Cons (mkVar 1,
-                               (mkShift2 2 (S.Cons (mkVar 4,
-                                                    mkShift2 0 S.Identity)
-                                           )
-                               )
-                              )
-                      )
-          )
-  )::
-  (S.Cons (mkVar 0,  (* Seems to work *)
-           mkShift2 3 (S.Cons (mkVar 1,
-                               (mkShift2 2 (S.Cons (mkVar 4,
-                                                    mkShift2 1 (S.Cons (mkVar 5,
-                                                                        mkShift2 0 S.Identity))
-                                                   )
-                                           )
-                               )
-                              )
-                      )
-          )
-  )::
+  (mkTestSubst ((0, 3)::(2, 2)::(3, 0)::[])):: (* Seems to work *)
+  (mkTestSubst ((1, 3)::(2, 2)::(3, 0)::[])):: (* Seems to work *)
+  (mkTestSubst ((4, 2)::(2, 2)::(3, 0)::[])):: (* Go completly wrong -> indices not in order -> should fail ?*)
+  (mkTestSubst ((1, 3)::(2, 2)::(4, 0)::[])):: (* Seems to work *)
+  (mkTestSubst ((0, 3)::(2, 2)::(4, 0)::[])):: (* Seems to work *)
+  (mkTestSubst ((0, 3)::(1, 2)::(4, 0)::[])):: (* Seems to work *)
+  (mkTestSubst ((0, 3)::(1, 2)::(4, 1)::(5, 0)::[]))::
+  (mkTestSubst ((0, 3)::(1, 2)::(4, 1)::(5, 0)::[]))::
   (S.Cons (mkVar 1, S.Shift(S.Identity, 3)))::
   []
 
 let lxp = mkVar 3
 
 let inputs = List.map (fun s ->
-    match inverse s with
-    | Some(s') -> (let lxp_inv = mkSusp lxp s'
+    match inverse s, flatten s with
+    | Some(s'), Some(sf) -> (let lxp_inv = mkSusp lxp s'
                    in let ret = (match unify lxp lxp_inv empty_subst with
                        | Some (_) -> true
                        | _ -> false)
-                   in let str = (string_of_subst s) ^ " -> " ^ (string_of_subst s')
+                   in let str = (string_of_subst sf) ^ (String.make (50 - String.length (string_of_subst sf)) ' ')
+                                                     ^  " -> " ^ (string_of_subst s')
                    in (ret, str))
-    | None -> (false, ((string_of_subst s) ^ " -> Non inversable")))
+    | _ -> (false, ((string_of_subst s) ^ " -> Non inversable")))
     input
 
 let _ = List.map (fun (res, str) -> add_test "INVERSE" str (fun () -> if res then success () else failure ()))
