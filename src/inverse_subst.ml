7798134b5936c6a11956fa962394691f28fa8cdb
n3f4s <tiv.crb@gmail.com>
2016-07-29 21:47:14 +0200

    add check for deBruijn indices >= shift offset

--- src/inverse_subst.ml
+++ src/inverse_subst.ml
@@ -25,7 +25,9 @@ let transfo (s: lexp S.subst) : substIR option =
     in match s with
     | S.Cons (Var _ as v, s) ->
       (match transfo s off_acc (idx + 1) with
-        | Some (tail, off) -> Some (((shiftVar v off_acc), idx)::tail, off)
+       | Some (tail, off) -> let newVar = shiftVar v off_acc
+         in if newVar >= off then None
+         else Some (((shiftVar v off_acc), idx)::tail, off)
         | None             -> None)
     | S.Shift (s, offset) -> transfo s (offset + off_acc) idx
     | S.Identity          -> Some ([], off_acc)
