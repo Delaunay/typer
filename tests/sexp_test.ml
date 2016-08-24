343fb9c2f9f6e6094c3ed44fc6f3e316b7dcc655
Stefan Monnier <monnier@iro.umontreal.ca>
2016-08-23 20:26:42 -0400

    * tests/sexp_test.ml (test_sexp_add): New function.  Use it.

--- tests/sexp_test.ml
+++ tests/sexp_test.ml
@@ -2,38 +2,26 @@ open Sexp
 open Lexer
 open Utest_lib
 
-let _ = (add_test "SEXP" "lambda x -> x + x" (fun () ->
+let test_sexp_add dcode testfun =
+  add_test "SEXP" dcode (fun () -> testfun (sexp_parse_str dcode))
 
-    let dcode = "lambda x -> x + x;" in
-
-    let ret = sexp_parse_str dcode in
+let _ = test_sexp_add "lambda x -> x + x" (fun ret ->
         match ret with
-            | [Node(lbd, [x; add])] -> (match lbd, x, add with
-                | (Symbol(_, "lambda_->_"), Symbol(_, "x"),
-                   Node(Symbol(_, "_+_"), [Symbol(_, "x"); Symbol(_, "x")])) -> success ()
-                | _ -> failure ())
-          | _ -> failure ()
-))
+        | [Node(Symbol(_, "lambda_->_"),
+                [Symbol(_, "x");
+                 Node(Symbol(_, "_+_"), [Symbol(_, "x"); Symbol(_, "x")])])]
+          -> success ()
+        | _ -> failure ()
+)
 
-let _ = (add_test "SEXP" "x * x * x" (fun () ->
-
-  let dcode = "x * x * x;" in
-
-  let ret = sexp_parse_str dcode in
-    match ret with
-      | [n] ->(match n with
-        | Node(Symbol(_, "_*_"),
-          [Node(Symbol(_, "_*_"), [Symbol(_, "x"); Symbol(_, "x")]); Symbol(_, "x")])
-            -> success ()
-
-        | Node(Symbol(_, "_*_"),
-          [Symbol(_, "x"); Node(Symbol(_, "_*_"), [Symbol(_, "x")]); Symbol(_, "x")])
-            -> success ()
-
-        | _ -> failure ())
-      | _ -> failure ()
-
-))
+let _ = test_sexp_add "x * x * x" (fun ret ->
+        match ret with
+        | [Node(Symbol(_, "_*_"),
+                [Node(Symbol(_, "_*_"), [Symbol(_, "x"); Symbol(_, "x")]);
+                 Symbol(_, "x")])]
+          -> success ()
+        | _ -> failure ()
+)
 
 (* run all tests *)
 let _ = run_all ()
