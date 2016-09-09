13b6dba762ef1d4587e64f27f70bbda8b2cd5d6e
n3f4s <tiv.crb@gmail.com>
2016-09-09 17:32:30 +0200

    add doc

--- src/debug_fun.ml
+++ src/debug_fun.ml
@@ -6,9 +6,11 @@ let _debug = true
 
 let logs = ref []
 
+(** Execute the function if _debug = true*)
 let do_debug func =
   if _debug then (func ()) else ()
 
+(** print the two string parameter (buffered print) *)
 let debug_print str1 str2 =
   logs := ((padding_left str1 20 ' ')^" : ", (str2^"\n"))::(!logs); ()
 
@@ -26,13 +28,16 @@ let clear_indent () =
       prerr_newline ();
       ())
 
+(** Print when _debug = true. No buffer *)
 let debug_print_no_buff str =
   do_debug (fun () -> prerr_string str; ())
 
+(** Print a lxp when _debug = true. No buffer *)
 let debug_print_lexp lxp =
   let str = colored_string_of_lxp lxp str_yellow str_magenta
   in do_debug (fun () -> prerr_string str; ())
 
+(** Helper for printing in a unify* function. Buffered *)
 let debug_print_unify fn lhs rhs str =
     let debug_print_unify fn lhs rhs str =
       let tmp = ((padding_left fn 20 ' ') ^ " : ",
